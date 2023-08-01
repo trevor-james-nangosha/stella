eb7b5fc9dbd7a366d5e4d551972f51c77f17f10d
trevor james nangosha <trevornangosha16@gmail.com>
2023-08-01 13:02:37 +0300

    second commit

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include "stdarg.h"
+
+int total(int num, ...){
+    va_list args;
+    va_start(args, num);
+
+    int total = 0;
+    for (int i = 0; i < num; ++i) {
+        total += va_arg(args, int);
+    }
+    va_end(args);
+    return total;
+}
 
 int main() {
-    statements();
+    // statements();
+    printf("The total is: %d", total(2, 4, 6));
 }
